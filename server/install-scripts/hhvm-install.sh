f794cba6a26928a5fe82f391afaefac92e736ed5
Ubuntu <ubuntu@ip-172-31-23-255.ap-southeast-1.compute.internal>
2017-05-27 13:16:57 +0000

    Added install scripts

---
+++ server/install-scripts/hhvm-install.sh
@@ -0,0 +1,9 @@
+sudo apt-get install software-properties-common
+
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
+sudo add-apt-repository "deb http://dl.hhvm.com/ubuntu $(lsb_release -sc) main"
+sudo apt-get update
+sudo apt-get install hhvm
+
+sudo update-rc.d hhvm defaults
+sudo /user/share/hhvm/install_fastcgi.sh
---
+++ server/install-scripts/php7-install.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get remove php5-common -y
+sudo apt-get purge php5-common -y
+sudo apt-get install php7.0 
+sudo apt-get install php-xml php-curl php-zip php-gd php-mbstring php-xmlrpc php-soap php-intl
