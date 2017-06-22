aac0a11d90f2a0b413e05832c63c862129b8be0c
root <root@ip-172-31-19-227.ap-southeast-1.compute.internal>
2017-06-22 17:20:39 +0000

    Changed docker start to use generic image

--- server/docker/docker-mk-webhost.sh
+++ server/docker/docker-mk-webhost.sh
@@ -1,2 +1,1 @@
-docker load < apache2php-dockerimg.tar
-docker run -it --name=webhost -p 80:80 -v /shared:/host apache2-php /bin/bash
+docker run -it --name=webhost -p 80:80 -v /shared:/shared ubuntu /bin/bash
