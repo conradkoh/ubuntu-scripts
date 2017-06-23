4b0816a2475a1714b35cbe0fe3227f5ccd1d64d5
conradkoh <conradkoh@gmail.com>
2017-06-23 19:53:15 +0000

    Added installation for mongdb

--- app/mongodb/mongo-install.sh
+++ app/mongodb/mongo-install.sh
@@ -1,4 +1,4 @@
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+apt-get update
+apt-get install mongodb-org -y
