3db7ea6485d051069d12c8aa52466c178f69fd55
conradkoh <conradkoh@gmail.com>
2017-06-26 08:05:23 -0700

    Updated transposer install scripts

---
+++ app/lamp-app-install.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get install vim
+./apache/apache2-install.sh #Apache 2
+./php/php7-install.sh #PHP 7
+./php/pecl-install.sh #install pecl for extension installation
+./php/php-mongo-install.sh #install mongo extension
+
--- development/transposer/docker-mk-transposer-app.sh
+++ development/transposer/docker-mk-transposer-app.sh
@@ -1,1 +1,1 @@
-docker run -it --name=TransposerAPP -p 80:80 -p 443:443 -v /mnt/data/transposer:/mnt/data/transposer -v /shared:/shared ubuntu /bin/bash
+docker run -it --name=TransposerAPP -p 80:80 -p 443:443 -v /mnt/data/transposer/transposer_data:/mnt/transposer_data -v /mnt/data/transposer/transposer_app:/var/www/html -v /shared:/shared ubuntu /bin/bash
