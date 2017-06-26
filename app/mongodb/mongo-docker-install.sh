faa146044ebe833e268110f5ef7f10bbffc526a0
conradkoh <conradkoh@gmail.com>
2017-06-26 07:39:02 -0700

    Updated mongo scripts

--- app/mongodb/mongo-docker-install.sh
+++ app/mongodb/mongo-docker-install.sh
@@ -6,4 +6,4 @@ apt-get install -y --no-install-recommends software-properties-common
 echo "deb http://repo.mongodb.org/apt/ubuntu $(cat /etc/lsb-release | grep DISTRIB_CODENAME | cut -d= -f2)/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update && apt-get install -y mongodb-org
 mkdir -p /data/db
-/usr/bin/mongod &
+/usr/bin/mongod & #start the service
--- development/transposer/docker-mk-transposer-db.sh
+++ development/transposer/docker-mk-transposer-db.sh
@@ -1,1 +1,1 @@
-docker run -it --name=TransposerDB -p 27017:27017 -p 27018:27018 -p 27019:27019 -p 28017:28017 -v /shared:/shared mongo /bin/bash
+docker run -it --name=TransposerDB -p 27017:27017 -p 27018:27018 -p 27019:27019 -p 28017:28017 -v /shared:/shared -v /mnt/data:/mnt/data mongo /bin/bash
