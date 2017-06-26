5e6eceb1e424f03c1f5e40999d8c188efec79ea7
conradkoh <conradkoh@gmail.com>
2017-06-26 06:57:53 -0700

    Fixed php mongo install

--- app/php/php-mongo-install.sh
+++ app/php/php-mongo-install.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install pkg-config -y
-apt-get install clibcurl4-openssl-dev pkg-config libssl-dev libsslcommon2-dev -y
+apt-get install libcurl4-openssl-dev pkg-config libssl-dev libsslcommon2-dev -y
 pecl install mongodb
 echo 'Installation completed. You should add "extension=mongodb.so" to php.ini';
 echo 'Get open the php.ini file? (y/n)'
