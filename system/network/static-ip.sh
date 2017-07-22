af0f87662bede308ae1cffda61be1d831c99eba4
conradkoh <conradkoh@gmail.com>
2017-07-22 10:05:06 -0700

    Fixed static ip breaking internet connection

--- system/network/static-ip.sh
+++ system/network/static-ip.sh
@@ -10,14 +10,11 @@ echo "Enter your interface name: ";
 read ifname
 echo "Enter the static ip: "
 read ipadd
-echo "Enter the gateway: "
-read gateway
 sudo cp /etc/network/interfaces /etc/network/interfaces-copy
 echo $'\n'
 echo "auto $ifname" >> $interface_file
 echo "iface $ifname inet static" >> $interface_file
 echo "address $ipadd" >> $interface_file
 echo "netmask 255.255.255.0" >> $interface_file
-echo "gateway $gateway" >> $interface_file
 sudo ifdown $ifname
 sudo ifup $ifname
