d1c9d7fa077ca76f01474f2d66d192e3c79f0c49
conradkoh <conradkoh@gmail.com>
2017-07-22 09:32:30 -0700

    Updated samba to smoothly install

--- app/samba/samba-install.sh
+++ app/samba/samba-install.sh
@@ -2,7 +2,7 @@
 if [ $(dpkg-query -W -f='${Status}' samba 2>/dev/null | grep -c "ok installed") ];
 then
 sudo apt-get update
-sudo apt-get install samba
+sudo apt-get install samba -y
 fi
 echo "Enter your username"
 read username
